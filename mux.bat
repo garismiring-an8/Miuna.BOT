126ea4a936d74b08beea2e0ab8cc01f230ccbc77
Padang Perwira Yudha <terrificsubs@gmail.com>
2018-04-02 01:08:56 +0700

    ....

--- mux.bat
+++
@@ -1,5 +0,0 @@
-"C:/Program Files/MKVToolNix\mkvmerge.exe" --ui-language en --output ^"E:\an8\temp\temp.mkv^" --no-subtitles --language 0:und --default-track 0:yes --language 1:jpn --default-track 1:yes ^"^(^" ^"E:\an8\temp\%~1^" ^"^)^" --language 0:und --track-name 0:GarisMiring-an8 --default-track 0:yes ^"^(^" ^"E:\an8\temp\%~2^" ^"^)^" --chapters ^"E:\an8\temp\%~3^" --track-order 0:0,0:1,1:0
-E:\an8\mux.py -i %1
-push_sett.bat
-twitter.py
-@pause
--- push_sett.bat
+++
@@ -1,8 +0,0 @@
-E:
-cd an8
-cd garismiring-an8.github.io
-git add .
-git commit -m "....."
-git push
-twitter.py
-@pause
